b5eca00486d847aa46f2697676fd7546e8a3712f
Omar <omar.thabet@vintus.nl>
2023-01-26 04:32:12 +0100

    progress

--- src/main/java/DynamicProgramming/SequenceAlignment.java
+++ src/main/java/DynamicProgramming/SequenceAlignment.java
@@ -1,10 +1,13 @@
 package DynamicProgramming;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class SequenceAlignment {
     public static int solve(String firstString, String secondString) {
         if (firstString.length() == 0) return secondString.length();
         if (secondString.length() == 0) return firstString.length();
-        int[][] mem = new int[firstString.length()+1][secondString.length()+1];
+        int[][] mem = new int[firstString.length() + 1][secondString.length() + 1];
         for (int i = 0; i < mem.length; i++) {
             mem[i][0] = i;
         }
@@ -28,6 +31,117 @@ public class SequenceAlignment {
                 mem[i][k] = Integer.min(matching, Integer.min(skippingLeft, skippingRight));
             }
         }
-        return mem[firstString.length()][secondString.length() ];
+        return mem[firstString.length()][secondString.length()];
+    }
+
+    public static int[][] createMemory(String firstString, String secondString) {
+        int[][] mem = new int[firstString.length() + 1][secondString.length() + 1];
+        for (int i = 0; i < mem.length; i++) {
+            mem[i][0] = i;
+        }
+
+        for (int i = 0; i < mem[0].length; i++) {
+            mem[0][i] = i;
+        }
+
+        for (int i = 1; i <= firstString.length(); i++) {
+            for (int k = 1; k <= secondString.length(); k++) {
+
+                var first = firstString.charAt(i - 1);
+                var second = secondString.charAt(k - 1);
+
+                var matching = (first == second ? 0 : 1) + mem[i - 1][k - 1];
+                var skippingLeft = 1 + mem[i - 1][k];
+                var skippingRight = 1 + mem[i][k - 1];
+//                mem[i][k] =
+//                        Integer.min(Integer.min(
+//                        (firstString.charAt(i-1) == secondString.charAt(k-1) ? 0 : 1) + mem[i - 1][k - 1], 1 + mem[i - 1][k]), 1 + mem[i][k - 1])
+                mem[i][k] = Integer.min(matching, Integer.min(skippingLeft, skippingRight));
+            }
+        }
+        return mem;
+    }
+
+    public static class Matching {
+        int l, r;
+
+        public Matching(int l, int r) {
+            this.l = l;
+            this.r = r;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + l + "," + r + ')';
+        }
+    }
+
+    public static class Matching2 {
+        char l, r;
+
+        public Matching2(char l, char r) {
+            this.l = l;
+            this.r = r;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + l + "," + r + ')';
+        }
+    }
+
+    public static List<Matching2> extractSolution(String firstString, String secondString) {
+        return extractSolution(createMemory(firstString, secondString), firstString, secondString);
+    }
+
+    public static List<Matching2> extractSolution(int[][] mem, String firstString, String secondString) {
+
+        var e = mem[firstString.length()][secondString.length()];
+        var res = new LinkedList<Matching>();
+        var res2 = new LinkedList<Matching2>();
+        int l = firstString.length();
+        int r = secondString.length();
+        while (l > 0 && r > 0) {
+            var match = (firstString.charAt(l - 1) == secondString.charAt(r - 1) ? 0 : 1);//+ mem[l - 1][r - 1];
+            var skipL = 1;//+ mem[l - 1][r];
+            var skipR = 1;//+ mem[l][r - 1];
+
+            var matched = mem[l - 1][r - 1];
+            var skippedL = mem[l - 1][r];
+            var skippedR = mem[l][r - 1];
+
+            if (mem[l][r] - match == mem[l - 1][r - 1]) {
+                res.addFirst(new Matching(l, r));
+                res2.addFirst(new Matching2(firstString.charAt(l - 1), secondString.charAt(r - 1)));
+
+                l--;
+                r--;
+            } else if (mem[l][r] - skipL == mem[l - 1][r]) {
+                res.addFirst(new Matching(l, -1));
+                res2.addFirst(new Matching2(firstString.charAt(l - 1), '-'));
+                l--;
+            } else if (mem[l][r] - skipR == mem[l][r - 1]) {
+                res.addFirst(new Matching(-1, r));
+
+                res2.addFirst(new Matching2('-', secondString.charAt(r - 1)));
+                r--;
+            }
+
+        }
+
+        while (l > 0) {
+            res.addFirst(new Matching(l, -1));
+            res2.addFirst(new Matching2(firstString.charAt(l - 1), '-'));
+            l--;
+        }
+
+        while (r > 0) {
+            res.addFirst(new Matching(-1, r));
+            res2.addFirst(new Matching2('-', secondString.charAt(r - 1)));
+            r--;
+        }
+
+        return res2;
+
     }
 }
--- src/test/java/DynamicProgramming/SequenceAlignmentTest.java
+++ src/test/java/DynamicProgramming/SequenceAlignmentTest.java
@@ -14,6 +14,8 @@ class SequenceAlignmentTest {
         String a = "kitten";
         String b = "sitting";
         Assertions.assertEquals(3, SequenceAlignment.solve(a, b));
+        Assertions.assertEquals("[(k,s), (i,i), (t,t), (t,t), (e,i), (n,n), (-,g)]", SequenceAlignment.extractSolution(a, b).toString());
+
     }
 
     @Test
@@ -22,6 +24,7 @@ class SequenceAlignmentTest {
         String a = "";
         String b = "";
         Assertions.assertEquals(0, SequenceAlignment.solve(a, b));
+        Assertions.assertEquals("[]", SequenceAlignment.extractSolution(a, b).toString());
     }
 
     @Test
@@ -30,6 +33,8 @@ class SequenceAlignmentTest {
         String a = "ab";
         String b = "";
         Assertions.assertEquals(2, SequenceAlignment.solve(a, b));
+        Assertions.assertEquals("[(a,-), (b,-)]", SequenceAlignment.extractSolution(a, b).toString());
+
     }
 
     @Test
@@ -38,6 +43,8 @@ class SequenceAlignmentTest {
         String a = "ab";
         String b = "a";
         Assertions.assertEquals(1, SequenceAlignment.solve(a, b));
+        Assertions.assertEquals("[(a,a), (b,-)]", SequenceAlignment.extractSolution(a, b).toString());
+
     }
 
     @Test
@@ -46,6 +53,8 @@ class SequenceAlignmentTest {
         String a = "ab";
         String b = "b";
         Assertions.assertEquals(1, SequenceAlignment.solve(a, b));
+        Assertions.assertEquals("[(a,-), (b,b)]", SequenceAlignment.extractSolution(a, b).toString());
+
     }
 
 
@@ -53,21 +62,28 @@ class SequenceAlignmentTest {
     @Timeout(100)
     public void example1() {
         String a = "abc";
-        String b = "a-bc";
-        Assertions.assertEquals(1, SequenceAlignment.solve(a, b));
+        String b = "azbc";
+        Assertions.assertEquals("[(a,a), (-,z), (b,b), (c,c)]", SequenceAlignment.extractSolution(a, b).toString());
+
     }
 
     @Test
     @Timeout(100)
     public void longTest() {
         Assertions.assertEquals(1, SequenceAlignment.solve("a", "b"));
+        Assertions.assertEquals("[(a,b)]", SequenceAlignment.extractSolution("a", "b").toString());
+
         Assertions.assertEquals(0, SequenceAlignment.solve("a", "a"));
+        Assertions.assertEquals("[(a,a)]", SequenceAlignment.extractSolution("a", "a").toString());
+
 
         Assertions.assertEquals(2, SequenceAlignment.solve("abcdef", "qbcdeq"));
+        Assertions.assertEquals("[(a,q), (b,b), (c,c), (d,d), (e,e), (f,q)]", SequenceAlignment.extractSolution("abcdef", "qbcdeq").toString());
 
-        Assertions.assertEquals(6, SequenceAlignment.solve("qwerty", "asdfgh"));
 
         Assertions.assertEquals(6, SequenceAlignment.solve("qwerty", "qwertyqwerty"));
+        Assertions.assertEquals("[(-,q), (-,w), (-,e), (-,r), (-,t), (-,y), (q,q), (w,w), (e,e), (r,r), (t,t), (y,y)]", SequenceAlignment.extractSolution("qwerty", "qwertyqwerty").toString());
+
 
     }
 
